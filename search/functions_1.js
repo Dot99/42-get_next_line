b0e52520a4066669517c68977d8e547fc78d3520
Dot99 <gui.rocha.felix@gmail.com>
2024-07-25 12:23:34 +0100

    Documentation

---
+++ search/functions_1.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['free_5fdata_0',['free_data',['../group__mandatory.html#gaa6c5d630c4c9b9f5a132246ca2a32e8f',1,'free_data(char *buffer, char *left_c):&#160;get_next_line.c'],['../group__bonus.html#ga7ab94d22619ad42574c3bd062eb61b39',1,'free_data(char *stash, char *buffer):&#160;get_next_line_bonus.c'],['../group__bonus.html#ga7ab94d22619ad42574c3bd062eb61b39',1,'free_data(char *stash, char *buffer):&#160;get_next_line.c']]],
+  ['ft_5fstrchr_1',['ft_strchr',['../group__mandatory.html#ga5c93631035858c3e4d554a173743ef94',1,'ft_strchr(char *str, char c):&#160;get_next_line_utils.c'],['../group__bonus.html#ga5c93631035858c3e4d554a173743ef94',1,'ft_strchr(char *str, char c):&#160;get_next_line_utils.c']]],
+  ['ft_5fstrclen_2',['ft_strclen',['../group__mandatory.html#ga4f273b84f2e5a38597adbd94d1fea5f1',1,'ft_strclen(char *str, char c):&#160;get_next_line_utils.c'],['../group__bonus.html#ga4f273b84f2e5a38597adbd94d1fea5f1',1,'ft_strclen(char *str, char c):&#160;get_next_line_utils.c']]],
+  ['ft_5fstrjoin_3',['ft_strjoin',['../group__mandatory.html#gab3e68fd1cdbf03a4982200495d4642d2',1,'ft_strjoin(char *s1, char *s2):&#160;get_next_line_utils.c'],['../group__bonus.html#gab3e68fd1cdbf03a4982200495d4642d2',1,'ft_strjoin(char *s1, char *s2):&#160;get_next_line_utils.c']]],
+  ['ft_5fstrndup_4',['ft_strndup',['../group__mandatory.html#ga642d7f3c5c7ee564a6de765134b233e0',1,'ft_strndup(char *s, int len):&#160;get_next_line_utils.c'],['../group__bonus.html#ga642d7f3c5c7ee564a6de765134b233e0',1,'ft_strndup(char *s, int len):&#160;get_next_line_utils.c']]]
+];
---
+++ search/functions_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['read_5ffrom_5ffile_0',['read_from_file',['../group__mandatory.html#gad14a84f29a67edf3ce5f4893fbf1857a',1,'read_from_file(int fd, char *stash):&#160;get_next_line_bonus.c'],['../group__mandatory.html#gad14a84f29a67edf3ce5f4893fbf1857a',1,'read_from_file(int fd, char *stash):&#160;get_next_line_bonus.c'],['../group__bonus.html#gad14a84f29a67edf3ce5f4893fbf1857a',1,'read_from_file(int fd, char *stash):&#160;get_next_line_bonus.c']]]
+];
